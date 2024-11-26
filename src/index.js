d6172d61f68c74247e58cfa8a9a4cff15a21bae9
sreekardev123 <sreesaikaranam27@gmail.com>
2024-11-26 15:50:51 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Table from './tables.js'
+import Main from './Maincomp';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Table/>
+    {/* <Navbarimages/> */}
+    <Main/>
+   
   </React.StrictMode>
 );
 
